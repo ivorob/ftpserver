68abaef99fc8b4ea944292bd2a70bfcb93920296
Ivan Vorobyev <ivorob@gmail.com>
2022-08-30 01:14:53 +0300

    Fix linux compilation issue

--- tests/BrowseDirectoryTest.cpp
+++ tests/BrowseDirectoryTest.cpp
@@ -27,7 +27,9 @@ struct dirent makeDirEntry(const std::string& name, uint8_t type)
     dirEntry.d_off = 0;
     dirEntry.d_reclen = sizeof(struct dirent);
     dirEntry.d_type = type;
+#if defined(__FreeBSD__) || defined(__APPLE_CC__)
     dirEntry.d_namlen = name.size();
+#endif
     strncpy(dirEntry.d_name, name.c_str(), sizeof(dirEntry.d_name));
     return dirEntry;
 }
